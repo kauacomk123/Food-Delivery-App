f3af9ca449eee68bba5596614bb18e6593819b74
kauacomk123 <kauasantana2707@gmail.com>
2025-12-16 20:07:45 -0300

    first commit

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../componests/Header/Header'
+import ExploreMenu from '../../componests/ExploreMenu/ExploreMenu'
+
+
+const Home = () => {
+
+    const [category,setCategory] = useState("All")
+
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+    </div>
+  )
+}
+
+export default Home
