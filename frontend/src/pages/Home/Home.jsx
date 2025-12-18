5ba8491f276c09ca235e482bb821d8ac82f9e769
kauacomk123 <kauasantana2707@gmail.com>
2025-12-17 22:10:13 -0300

    novas pastas e arquivos

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../componests/Header/Header'
 import ExploreMenu from '../../componests/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../componests/FoodDisplay/FoodDisplay'
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
